5993afd41d584d8cfb609d8fab186d76600c4b8f
Fedor Alekseev <feodor.alexeev@gmail.com>
2018-11-27 03:16:13 +0300

    Add gauss

---
+++ content/numerical/SolveLinear.h
@@ -0,0 +1,51 @@
+/**
+ * Author: Per Austrin, Simon Lindholm, with style fixes by Fedor Alekseev
+ * Date: 2004-02-08
+ * License: CC0
+ * Description: Solves $A * x = b$. If there are multiple solutions, an arbitrary one is returned.
+ *  Returns rank, or -1 if no solutions. Data in $A$ and $b$ is lost.
+ * Time: O(n^2 m)
+ * Status: tested on kattis:equationsolver, and bruteforce-tested mod 3 and 5 for n,m <= 3
+ */
+#pragma once
+
+int solveLinear(vector<vector<double>& A, vector<double>& b, vector<double& x) {
+	int n = sz(A), m = sz(x), rank = 0, br, bc;
+	if (n) assert(sz(A[0]) == m);
+	vector<int> col(m); iota(WHOLE(col), 0);
+
+	for (int i = 0; i < n; ++i) {
+		double v, bv = 0;
+		for (int r = i; r < n; ++r) for (int c = i; c < m; ++c)
+			if ((v = fabs(A[r][c])) > bv)
+				br = r, bc = c, bv = v;
+		if (bv < eps) {
+			for (int j = 0; j < n; ++j)
+				if (fabs(b[j]) > eps)
+					return -1;
+			break;
+		}
+		swap(A[i], A[br]);
+		swap(b[i], b[br]);
+		swap(col[i], col[bc]);
+		for (int j = 0; j < n; ++j)
+			swap(A[j][i], A[j][bc]);
+		bv = 1. / A[i][i];
+		for (int j = i + 1; j < n; ++j) {
+			double fac = A[j][i] * bv;
+			b[j] -= fac * b[i];
+			for (int k = i + 1; k < m; ++k)
+				A[j][k] -= fac * A[i][k];
+		}
+		rank++;
+	}
+
+	x.assign(m, 0);
+	for (int i = rank; i--;) {
+		b[i] /= A[i][i];
+		x[col[i]] = b[i];
+		for (int j = 0; j < i; ++j)
+			b[j] -= A[j][i] * b[i];
+	}
+	return rank; // (multiple solutions if rank < m)
+}
